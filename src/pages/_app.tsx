46ec8f4206bce47b94162828b93951a36037cb1b
Asura146 <kyo008g@gmail.com>
2025-03-29 19:28:01 +0900

    tailwindcssの初期設定

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import { SessionProvider } from 'next-auth/react';
 import type { AppProps } from 'next/app';
+import '../styles/globals.css';
 
 export default function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
