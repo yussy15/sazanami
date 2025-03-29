f4dc27103a121465bb02b8ecff20f34c62644a89
Asura146 <kyo008g@gmail.com>
2025-03-30 02:16:46 +0900

    HederのUIを作成

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,26 @@
-import Link from 'next/link';
+import Link from "next/link";
 
 const Header = () => {
   return (
-    <header className="bg-blue-500 p-4 text-white">
-      <nav>
+    <header className="bg-white shadow-lg p-4 text-white flex items-center">
+      <img src="/sazanami_dev.svg" alt="" className="h-10" />
+      <nav className="ml-auto">
         <ul className="flex space-x-4">
-          <li><Link href="/">ホーム</Link></li>
-          <li><Link href="/about">About</Link></li>
-          <li><Link href="/contact">Contact</Link></li>
+          <li>
+            <Link href="/Top" className="text-gray-500  rounded-lg p-2 hover:bg-black hover:text-white transition duration-500 ease-in-out">
+              <span className="mx-3">Top</span>
+            </Link>
+          </li>
+          <li>
+            <Link href="/about" className="text-gray-500  rounded-lg p-2 hover:bg-black hover:text-white transition duration-300 ease-in-out">
+            <span className="mx-3">Member</span>
+            </Link>
+          </li>
+          <li>
+            <Link href="/contact" className="text-gray-500  rounded-lg p-2 hover:bg-black hover:text-white transition duration-300 ease-in-out">
+            <span className="mx-3">Project</span>
+            </Link>
+          </li>
         </ul>
       </nav>
     </header>
