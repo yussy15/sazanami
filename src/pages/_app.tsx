42272bfc41dd4df45d69997145eefcd209eb37c5
Asura146 <kyo008g@gmail.com>
2025-03-30 02:56:39 +0900

    ヘッダーを特定ページで表示させないようにした

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,23 @@
-import { SessionProvider } from 'next-auth/react';
-import type { AppProps } from 'next/app';
-import '../styles/globals.css';
-import Header from '../components/Header';
+import { SessionProvider } from "next-auth/react";
+import type { AppProps } from "next/app";
+import { useRouter } from "next/router";
+import "../styles/globals.css";
+import Header from "../components/Header";
 
-export default function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+export default function MyApp({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps) {
+  const router = useRouter();
+
+  // index.tsx (ルートパス) と Signup ページでヘッダーを非表示にしたい例
+  const hideHeaderPaths = ["/", "/Signup"];
+  const shouldShowHeader = !hideHeaderPaths.includes(router.pathname);
+
   return (
-    <>
-      <Header />
-      <SessionProvider session={session}>
-        <Component {...pageProps} />
-      </SessionProvider>
-    </>
+    <SessionProvider session={session}>
+      {shouldShowHeader && <Header />}
+      <Component {...pageProps} />
+    </SessionProvider>
   );
 }
