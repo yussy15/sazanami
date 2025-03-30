0dbc951623e291ca868dca3ea4eca36c8a11b56e
andooo26 <yota426513@gmail.com>
2025-03-31 02:47:34 +0900

    俺らの最高の過ち

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -5,7 +5,7 @@ export default NextAuth({
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
-      clientSecret: "0TyTuCiRKZO2sDMv2fGPnRRKcu1CDMnojKxRQWSFss4=",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
   ],
   callbacks: {
