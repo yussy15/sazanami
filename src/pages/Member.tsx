e670b46565a1dc51be1b4db646daa7ab293aa95b
ENE-Acorn <111696397+ENE-Acorn@users.noreply.github.com>
2025-03-31 12:28:49 +0900

    roleによる表示制御

--- src/pages/Member.tsx
+++ src/pages/Member.tsx
@@ -5,7 +5,11 @@ import { withAuth } from '../utils/withAuth';
 type Member = {
   name: string;
   email: string;
-};
+  class_name: string;
+  class_number: string;
+  student_id: string;
+  role: string;
+}
 
 function MemberPage() {
   const [members, setMembers] = useState<Member[]>([]);
@@ -28,14 +32,17 @@ function MemberPage() {
   if (!session) {
     return <p>認証されていません。ログインしてください。</p>;
   }
-
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 space-y-4">
+    <div className="mt-20 flex flex-col items-center justify-center min-h-screen bg-gray-100 space-y-4">
       <h1 className="text-xl font-bold mb-4">Member Page</h1>
       {members.map((member, index) => (
-        <div key={index} className="bg-white p-4 shadow-md rounded w-1/2">
+        <div key={index} className="bg-white p-4 shadow-md rounded w-full">
           <p>名前: {member.name}</p>
-          <p>メール: {member.email}</p>
+          <p>{(member.email)? `メール:${member.email}`:<div></div>}</p>
+          <p>{(member.class_name)? `クラス名:${member.class_name}`:<div></div>}</p>
+          <p>{(member.class_number)? `クラス番号:${member.class_number}`:<div></div>}</p>
+          <p>{(member.student_id)? `学籍番号:${member.student_id}`:<div></div>}</p>
+          <p>{(member.role)? `ロール:${member.role}`:<div></div>}</p>
         </div>
       ))}
     </div>
