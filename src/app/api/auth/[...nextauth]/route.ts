100db72bab4d5229899053ce72821d9a928113ea
ENE-Acorn <111696397+ENE-Acorn@users.noreply.github.com>
2025-04-06 07:39:46 +0900

    なくても動いたのでNEXTAUTHを削除

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -2,7 +2,6 @@ import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
 export const authOptions = {
-  secret: process.env.NEXTAUTH_SECRET,
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
