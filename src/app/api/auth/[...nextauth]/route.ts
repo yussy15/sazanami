c4c3182154068f52ca840d17401dd6d4a62cd659
ENE_Acorn <111696397+ENE-Acorn@users.noreply.github.com>
2025-04-06 22:30:32 +0900

    やっぱりNEXTAUTHがないと動かないので追加

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -2,6 +2,7 @@ import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
 export const authOptions = {
+  secret: process.env.NEXTAUTH_SECRET,
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
