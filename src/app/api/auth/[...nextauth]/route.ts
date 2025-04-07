b5fb30ca1ccd0335bd8d31ef771fdc1c7f11d3a9
andooo26 <yota426513@gmail.com>
2025-04-07 23:33:11 +0900

    nextauthを2ファイルに分けてみる(これが推奨みたい)

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,20 +1,6 @@
 import NextAuth from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
+import { authOptions } from "../../../../authOptions";
 
-export const authOptions = {
-  secret: process.env.NEXTAUTH_SECRET,
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID!,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-    }),
-  ],
-  callbacks: {
-    async signIn({ user }) {
-      const domain = user.email?.split("@")[1];
-      return domain === process.env.AUTHORIZED_DOMAIN;
-    },
-  },
-};
+const handler = NextAuth(authOptions);
 
-export default NextAuth(authOptions);
+export { handler as GET, handler as POST };
---
+++ src/authOptions.ts
@@ -0,0 +1,18 @@
+import { NextAuthOptions } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+
+export const authOptions: NextAuthOptions = {
+  secret: process.env.NEXTAUTH_SECRET,
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+  ],
+  callbacks: {
+    async signIn({ user }) {
+      const domain = user.email?.split("@")[1];
+      return domain === process.env.AUTHORIZED_DOMAIN;
+    },
+  },
+};
